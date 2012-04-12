a89dcfa92201e48f17649fc016a7883331cb32dd
Paul Hammant <paul@hammant.org>
2012-04-11 19:58:09 -0500

    no more fluent selenium (for speed)

--- knockout.groovy
+++ knockout.groovy
@@ -1,6 +1,5 @@
 @Grapes([
  @Grab("org.seleniumhq.selenium:selenium-java:2.20.0"),
- @Grab("org.seleniumhq.selenium.fluent:fluent-selenium:1.6.2"), 
  @Grab("org.hamcrest:hamcrest-all:1.1"),
  @GrabExclude('xml-apis:xml-apis')
 ])
@@ -9,23 +8,16 @@ import org.openqa.selenium.By
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.Keys
 import org.openqa.selenium.StaleElementReferenceException
-import org.seleniumhq.selenium.fluent.FluentWebDriverImpl
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertThat;
-import static org.seleniumhq.selenium.fluent.FluentBy.attribute
-import static org.seleniumhq.selenium.fluent.FluentBy.last
-import static org.seleniumhq.selenium.fluent.Period.secs
 
 
 def driver = new FirefoxDriver()
-def fluent = new FluentWebDriverImpl(driver)
 
 driver.get "http://paul-hammant.github.com/knockout_todo_app/"
 
-def form = fluent.within(secs(10)).div(By.id("todoapp"))
-
-def todoField = form.input(By.xpath("contains(@data-bind, 'enterKey: add')"))
+def todoField = driver.findElement(By.xpath("//input[contains(@data-bind, 'enterKey: add')]"))
 
 println "iteration,duration,findLastItemDuration,getItemText"
 for ( i in 1..1000 ) {
